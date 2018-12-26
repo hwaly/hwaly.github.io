e5d43168ecfdd268b422404ea01003e251c12430
Byeonghwa.Yeon <hwalybell@gmail.com>
2018-12-27 01:03:11 +0900

    수정 - 레이아웃

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+---
+---
+
+const $ = (selector, context = document) => context.querySelector(selector);
+const $$ = (selector, context = document) => Array.from(context.querySelectorAll(selector));
+const $on = (target, type, callback, capture) => target.addEventListener(type, callback, !!capture);
+
+const scrollSiteMenu = () => {
+    const header = $('.site-header');
+    const onScroll = () => {
+        console.log(window.pageYOffset);
+        header.classList[window.pageYOffset ? 'add' : 'remove']('site-header--fixed');
+    };
+
+    $on(window, 'scroll', onScroll);
+};
+
+const app = () => {
+    scrollSiteMenu();
+};
+
+$on(document, 'DOMContentLoaded', app);
